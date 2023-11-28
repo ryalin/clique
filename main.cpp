203f2fad8210b09781ff5baf0fa1716692b8b445
Ryan Lin <rlin2@andrew.cmu.edu>
2023-11-28 18:10:21 -0500

    changed test file to set

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <set>
 
 #include "test.h"
 #include "sequential.h"
@@ -10,15 +11,15 @@ int main(int argc, const char **argv) {
   // Read input parameters:
   // - graph size, how many cores, etc.
 
-  std::map< int,std::vector<int> > graph = generateGraph(3, 5);
+  std::map< int,std::set<int>> graph = generateOneClique(50, 56);
   for (int i = 1; i < graph.size() + 1; i++) {
     std::cout << "NODE " << i  << ": "<< std::endl;
-    for (int j = 0; j < graph[i].size(); j++) {
-      std::cout << " " << graph[i][j] << " " << std::endl;
+    for (auto elem: graph[i]) {
+      std::cout << " " << elem << " " << std::endl;
     }
   }
-  std::cout << "poop" << std::endl;
-  // bool isClique = sequentialClique(graph, 3);
+  // std::cout << "poop" << std::endl;
+  // bool isClique = sequentialClique(graph, 25);
   // std::cout << "result: " << isClique << std::endl;
   // Call graph generation
   // Call sequential algorithm (return timing)
